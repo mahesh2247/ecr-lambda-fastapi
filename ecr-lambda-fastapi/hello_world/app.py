ae22a0055984a3879a515fdd2264534ede781194
mahesh2247 <maheshm.mca18@bmsce.ac.in>
2023-11-24 17:52:46 +0530

    added get method for accepting args

--- ecr-lambda-fastapi/hello_world/app.py
+++ ecr-lambda-fastapi/hello_world/app.py
@@ -1,5 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from mangum import Mangum
 import uvicorn
 import json
@@ -8,11 +9,11 @@ app = FastAPI()
 
 @app.get("/hello")
 async def root():
-    return {"message`": "Hello World!"}
+    return {"message`": "Hello REST API Demo using FastAPI deployed on Lambda!"}
 
-@app.get("/secondgreet")
-async def second_root():
-    return {"message`": "Hello Developer!"}
+@app.get("/{name}")
+async def intro(name: str):
+    return JSONResponse({"message`": f"Hello {name}! Nice to meet you"})
 
 handler = Mangum(app)
 
